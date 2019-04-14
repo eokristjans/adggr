996d82cc7410fe8836b6ee7981ead6e099698662
Davíð Freyr Björnsson <david24099@gmail.com>
2019-04-14 18:30:18 +0000

    Drög í gognin.R
    Drög að leigu/greiðsluvilja histogrami

--- Lokaverkefni-Husnaedisval/gognin.R
+++ Lokaverkefni-Husnaedisval/gognin.R
@@ -1,3 +1,4 @@
+rm(list = ls())
 library(ggplot2)
 library(Hmisc)
 
@@ -25,4 +26,31 @@ ggplot(gogn, aes(x=gaedi, color=type, fill=type)) +
         legend.title = element_blank())
 
   ggsave("krofur_vs_eign.png")
+  
+  # Leiga er 0.5% af núvirði eignar
+  gogn$leiga <- 5*gogn$nuvirdi
+  
+  leiga <- cbind(gogn$leiga[gogn$type == "  Eign"],
+                 "leiga")
+  greidslugeta <- cbind(gogn$greidslugeta[gogn$type == "  Kröfur kúnna"],
+                        "greidslugeta")
+
+  payment <- rbind(leiga, greidslugeta)
+  names(payment) <- c("amount", "type")
+  
+  payment$amount <- as.numeric(payment$amount)
+
+  
+ggplot(payment, aes(x=amount, color=type, fill=type)) +
+  geom_histogram(binwidth = 10000, 
+                 alpha=0.55, 
+                 position="identity") +
+  scale_x_continuous(breaks = seq(0, 500000, 10000)) +
+  scale_y_continuous(breaks = seq(0, 2000, 100)) +
+  xlab("Gæði") +
+  ylab("Fjöldi") +
+  ggtitle("Gæði eigna og gæðakröfur kúnna") +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.title = element_blank())
+
 
