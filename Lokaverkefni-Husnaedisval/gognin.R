dc520450acf341af0a842b206bc2dde030fc687a
Davíð Freyr Björnsson <david24099@gmail.com>
2019-04-14 23:53:36 +0000

    rewrite

--- Lokaverkefni-Husnaedisval/gognin.R
+++ Lokaverkefni-Husnaedisval/gognin.R
@@ -1,56 +1,51 @@
 rm(list = ls())
 library(ggplot2)
-library(Hmisc)
+library(scales)
 
+# Lesum inn gögnin
 eignir <- read.csv("./GognErling/eignir.txt", header=TRUE, sep="")
 kunnar <- read.csv("./GognErling/kunnar.txt", header=TRUE, sep="")
-gogn <- merge(eignir, kunnar, by = "gaedi", all=TRUE)
 
-gogn$type[is.na(gogn$nrkunna)] = "eign"
-gogn$type[!is.na(gogn$nrkunna)] = "kunni"
-
-gogn$type = factor(gogn$type, 
-                      levels = c("eign", "kunni"),
-                      labels = c("  Eign", "  Kröfur kúnna"))
-
-ggplot(gogn, aes(x=gaedi, color=type, fill=type)) +
-  geom_histogram(binwidth = 0.2, 
-                 alpha=0.55, 
-                 position="identity") +
-  scale_x_continuous(breaks = seq(0, 5, 0.4)) +
-  scale_y_continuous(breaks = seq(0, 2000, 100)) +
+# Teiknum tíðnirit (histogram) fyrir gæði húss
+# og gæðakröfur kúnna
+ggplot(eignir, aes(x = gaedi)) +
+  geom_histogram(data = kunnar,
+                 binwidth = 0.2,
+                 fill = "red",
+                 alpha = 0.3) +
+  geom_histogram(data = eignir,
+                 binwidth = 0.2,
+                 fill = "blue",
+                 alpha = 0.3) + 
+  scale_x_continuous(breaks = seq(min(min(eignir$gaedi, kunnar$gaedi)), 
+                                  5, 0.2)) +
+  scale_y_continuous(breaks = seq(0, 3000, 100)) +
   xlab("Gæði") +
   ylab("Fjöldi") +
   ggtitle("Gæði eigna og gæðakröfur kúnna") +
-  theme(plot.title = element_text(hjust = 0.5),
-        legend.title = element_blank())
+  theme(plot.title = element_text(hjust = 0.5))
 
-  ggsave("krofur_vs_eign.png")
+# Vistum útkomuna
+ggsave("krofur_vs_eign.png")
+
+# Leiga er 0.5% af núvirði eignar
+eignir$greidslugeta <- 5*eignir$nuvirdi
+
+# Teiknum tíðnirit (histogram) fyrir leigu eignar
+# og greiðsluvilja kúnna
+ggplot(eignir, aes(x = greidslugeta)) +
+  geom_histogram(data = kunnar,
+                 binwidth = 50000,
+                 fill = "red",
+                 alpha = 0.3) +
+  geom_histogram(data = eignir,
+                 binwidth = 50000,
+                 fill = "blue",
+                 alpha = 0.3) +
+  scale_x_continuous(breaks = seq(0, 
+                                  max(max(eignir$greidslugeta),
+                                      max(kunnar$greidslugeta)),
+                                  100000)) + 
+  scale_y_continuous(breaks = seq(0, 10000, 500))
   
-  # Leiga er 0.5% af núvirði eignar
-  gogn$leiga <- 5*gogn$nuvirdi
   
-  leiga <- cbind(gogn$leiga[gogn$type == "  Eign"],
-                 "leiga")
-  greidslugeta <- cbind(gogn$greidslugeta[gogn$type == "  Kröfur kúnna"],
-                        "greidslugeta")
-
-  payment <- rbind(leiga, greidslugeta)
-  names(payment) <- c("amount", "type")
-  
-  payment$amount <- as.numeric(payment$amount)
-
-  
-ggplot(payment, aes(x=amount, color=type, fill=type)) +
-  geom_histogram(binwidth = 10000, 
-                 alpha=0.55, 
-                 position="identity") +
-  scale_x_continuous(breaks = seq(0, 500000, 10000)) +
-  scale_y_continuous(breaks = seq(0, 2000, 100)) +
-  xlab("Gæði") +
-  ylab("Fjöldi") +
-  ggtitle("Gæði eigna og gæðakröfur kúnna") +
-  theme(plot.title = element_text(hjust = 0.5),
-        legend.title = element_blank())
-
-
